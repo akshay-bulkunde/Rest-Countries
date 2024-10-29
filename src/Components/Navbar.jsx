a64191accefe71d4abad4b709027bfdfa03288a6
akshay-bulkunde <akshay.bulkunde.29.3@mountblue.tech>
2024-10-29 16:00:24 +0530

    Fix : Add moon and light icon

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import { useTheme } from '../Context/Theme'
 
 const Navbar = () => {
-  const [theme,toggleTheme] = useTheme();
-
+  const [theme, toggleTheme] = useTheme();
+  console.log(theme)
 
   return (
     <div className='py-5 shadow-xl px-8 flex justify-between header'>
-        <h1 className=' font-bold'>Where in the world?</h1>
-        <button className='font-bold' onClick={toggleTheme}>Dark</button>
+      <h1 className=' font-bold'>Where in the world?</h1>
+      <button className='font-bold' onClick={toggleTheme}>
+        {theme === 'light-theme' ? <i class="fa-regular fa-moon"></i> : <i class="fa-regular fa-sun"></i>}
+
+        {theme === 'light-theme' ? " Dark mode" : " Light Mode"}
+      </button>
     </div>
   )
 }
